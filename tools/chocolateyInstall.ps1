577e0a7982454d94363768e4e16e7da699c2daec
Ash Caire <ash.caire@gmail.com>
2016-02-18 10:40:46 +1100

    bump version to 0.4.4.19

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName = 'NSClient++'
 $installerType = 'msi'
-$url = 'http://files.nsclient.org/legacy/NSCP-0.4.1.102-Win32.msi'
-$url64 = 'http://files.nsclient.org/legacy/NSCP-0.4.1.102-x64.msi'
+$url = 'https://github.com/mickem/nscp/releases/download/0.4.4.19/NSCP-0.4.4.19-Win32.msi'
+$url64 = 'https://github.com/mickem/nscp/releases/download/0.4.4.19/NSCP-0.4.4.19-x64.msi'
 
-$silentArgs = '/quiet' 
-$validExitCodes = @(0) 
+$silentArgs = '/quiet'
+$validExitCodes = @(0)
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
