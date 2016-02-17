eda3c2a6ae04f722bdeac13b606d030531b1f6b7
Ash Caire <ash.caire@gmail.com>
2016-02-18 10:36:13 +1100

    import nuspec and powershell from package

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = 'NSClient++'
+$installerType = 'msi'
+$url = 'http://files.nsclient.org/legacy/NSCP-0.4.1.102-Win32.msi'
+$url64 = 'http://files.nsclient.org/legacy/NSCP-0.4.1.102-x64.msi'
+
+$silentArgs = '/quiet' 
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
